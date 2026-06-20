f3b87f66b4492d4a2d10dc3ec638b9fbaaf4bb86
Danonienko <danonienko8@gmail.com>
2026-06-21 01:44:53 +0300

    chore(Humanoid.iris): Remove `jumpPowerState`

--- src/Client/UI/Iris/Components/Character/Humanoid.iris.ts
+++ src/Client/UI/Iris/Components/Character/Humanoid.iris.ts
@@ -13,16 +13,12 @@ export default function HumanoidIris(humanoid: Humanoid): void {
 	const walkSpeedState = Iris.State(humanoid.WalkSpeed);
 	walkSpeedState.onChange((value) => (humanoid.WalkSpeed = value));
 
-	const jumpPowerState = Iris.State(humanoid.JumpPower);
-	jumpPowerState.onChange((value) => (humanoid.JumpPower = value));
-
 	Iris.CollapsingHeader(["Humanoid"]);
 	{
 		Iris.ProgressBar(["Health"], { progress: healthStateProgress });
 		Iris.SliderNum(["Current Health"], { number: currentHealthState });
 		Iris.InputNum(["Max Health"], { number: maxHealthState });
 		Iris.InputNum(["Walk Speed"], { number: walkSpeedState });
-		Iris.InputNum(["Jump Power"], { number: jumpPowerState });
 	}
 	Iris.End();
 }
