288253c8f64cd027fb066ff46ba50bbb8c13b267
Danonienko <danonienko8@gmail.com>
2026-06-21 01:46:34 +0300

    chore(Signals.ts): Divide signals o `Client` and `Server`

--- src/Shared/Signals.ts
+++ src/Shared/Signals.ts
@@ -1,9 +1,12 @@
 const Signals = {
-	Cmdr: {
-		ConnectDebugPanel: new Instance("BindableFunction") as BindableFunction<
-			() => [success: boolean, result: string]
-		>
-	}
+	Client: {
+		Cmdr: {
+			ConnectDebugPanel: new Instance("BindableFunction") as BindableFunction<
+				() => [success: boolean, result: string]
+			>
+		}
+	},
+	Server: {}
 } as const;
 
 export default Signals;
